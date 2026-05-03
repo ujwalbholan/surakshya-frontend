ae70d288065a9b243f00ebc2a290341d28be299a
ujwal bholan <160625871+ujwalbholan@users.noreply.github.com>
2026-05-03 13:03:40 +0545

    shade cn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
 export default function Home() {
   return (
-    <div className="w-scree  h-screen flex justify-center items-center">
+    <div className="w-scree  h-screen flex justify-center items-center flex-col mt-10">
       <h1 className="font-bold text-3xl underline italic">
         Surakshya Frontend
       </h1>
+      <Button
+        className="p-4 rounded-lg"
+        onClick={() => toast.success("Event has been created")}
+      >
+        Click
+      </Button>
     </div>
   );
 }
